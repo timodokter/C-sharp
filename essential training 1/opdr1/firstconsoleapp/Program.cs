893715cb89c594c2df3e6e259332d6a52a2632a7
Timo Dokter <timo.dokter@student.rocvf.nl>
2021-01-18 09:47:22 +0100

    ik heb het bestand voor mijn eerste consoleapp gemaakt

---
+++ essential training 1/opdr1/firstconsoleapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace firstconsoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
